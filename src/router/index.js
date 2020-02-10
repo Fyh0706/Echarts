46e2736710f032544c1809bf51dfce3f15aed2c0
Fyh0706 <54092244+Fyh0706@users.noreply.github.com>
2020-02-10 15:35:20 +0800

    echarts

---
+++ src/router/index.js
@@ -0,0 +1,52 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+// import Home from '../views/Home.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    redirect: '/yqdt',
+    component:()=>import(/* webpackChunkName: "INDEX" */ '../views/index.vue'),
+    children: [
+      {
+        path: '/yqdt',
+        name: 'yqdt',
+        component: () => import(/* webpackChunkName: "yqdt" */ '../components/yqdt')
+      },
+      {
+        path: '/qxmap',
+        name: 'qx',
+        component: () => import(/* webpackChunkName: "qx" */ '../components/qxmap')
+      },
+      {
+        path: '/hotsearch',
+        name: 'hotsearch',
+        component: () => import(/* webpackChunkName: "hotsearch" */ '../components/hotsearch')
+      },
+      {
+        path: '/news',
+        name: 'news',
+        component: () => import(/* webpackChunkName: "news" */ '../components/news')
+      }
+    ]
+  },
+  {
+    path:'/login',
+    component:()=>import(/* webpackChunkName: "LOGIN" */ '../views/login.vue')
+  },
+  {
+    path: '*',
+    redirect: "/yqdt"
+  }
+
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
